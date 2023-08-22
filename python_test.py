f944ae981bbbc774674ed3400ea2d8944b0ed65c
aronorriv <103250823+aronorriv@users.noreply.github.com>
2023-08-22 11:06:19 +0000

    Create python_test.py

---
+++ python_test.py
@@ -0,0 +1,13 @@
+one = int(input("Fyrsta talan:"))
+two = int(input("Önnur talan:"))
+three = int(input("Þriðja talan:"))
+
+def add(a,b,c):
+    sum = a + b + c
+    print("Halló Stærðfræði", sum)
+
+add(one, two, three)
+
+#htala = one + two + three
+
+#print (f"{htala} Halló Stærðfræði")
